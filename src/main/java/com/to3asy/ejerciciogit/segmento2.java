ca4f76a952f0b4edeae8b7d065a03043dc936b15
Henry Albeiro Montoya Jimenez <henry.montoya@sophossolutions.com>
2021-05-23 16:26:54 -0500

    Caso de uso numero 001

---
+++ src/main/java/com/to3asy/ejerciciogit/segmento2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.to3asy.ejerciciogit;
+
+/**
+ *
+ * @author henry.montoya
+ */
+public class segmento2 {
+    
+    public void degunda_clase(){
+        System.out.println("Este es el mensaje de la segunda clase");
+    }
+}
