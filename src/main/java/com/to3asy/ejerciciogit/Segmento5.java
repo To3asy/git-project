a71ba17a08dbb71cfd59cd1c577dc3c4ae122dde
Henry Albeiro Montoya Jimenez <henry.montoya@sophossolutions.com>
2021-05-23 17:11:43 -0500

    Commit del CU004

---
+++ src/main/java/com/to3asy/ejerciciogit/Segmento5.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.to3asy.ejerciciogit;
+
+/**
+ *
+ * @author henry.montoya
+ */
+public class Segmento5 {
+    
+    public void segmento_5(){
+        System.out.println("Mensaje del segmento 5");
+    }
+}
