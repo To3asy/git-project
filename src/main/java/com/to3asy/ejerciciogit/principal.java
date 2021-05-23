c7cd419b500aa58315f522298c8ec6f3311b8596
Henry Albeiro Montoya Jimenez <henry.montoya@sophossolutions.com>
2021-05-23 15:38:30 -0500

    Commit inicial

---
+++ src/main/java/com/to3asy/ejerciciogit/principal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.to3asy.ejerciciogit;
+
+/**
+ *
+ * @author henry.montoya
+ */
+public class principal {
+    
+    public void mensaje(){
+        System.out.println("Este es mi primer mensaje");
+    }
+}
