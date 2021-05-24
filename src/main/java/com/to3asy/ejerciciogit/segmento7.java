3d624df56ca2b81e22c61b12450e71d7cb3ab925
Henry Albeiro Montoya Jimenez <henry.montoya@sophossolutions.com>
2021-05-23 19:04:17 -0500

    commit del caso de uso 007

---
+++ src/main/java/com/to3asy/ejerciciogit/segmento7.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.to3asy.ejerciciogit;
+
+/**
+ *
+ * @author henry.montoya
+ */
+public class segmento7 {
+    
+    public void segmento_7(){
+        System.out.println("Mensaje del segmento 7");
+    }
+}
