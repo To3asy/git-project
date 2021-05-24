501dbbb3c6c1253eec0856cc97c6e3872caffadc
Henry Albeiro Montoya Jimenez <henry.montoya@sophossolutions.com>
2021-05-23 19:01:20 -0500

    commit segmento 6

---
+++ src/main/java/com/to3asy/ejerciciogit/segmento6.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.to3asy.ejerciciogit;
+
+/**
+ *
+ * @author henry.montoya
+ */
+public class segmento6 {
+    
+    public void segmento6(){
+        System.out.println("Mensaje Segmeno 6");
+    }
+}
