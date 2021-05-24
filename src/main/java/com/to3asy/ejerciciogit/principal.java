eff98eaa619df96f12daae66e85dc9e34c6d7932
Henry Albeiro Montoya Jimenez <henry.montoya@sophossolutions.com>
2021-05-24 09:02:18 -0500

    Correccion del error

--- src/main/java/com/to3asy/ejerciciogit/principal.java
+++ src/main/java/com/to3asy/ejerciciogit/principal.java
@@ -14,6 +14,6 @@ public class principal {
     public void mensaje(){
         System.out.println("Este es mi primer mensaje");
         System.out.println("Este es el segundo Mensaje");
-        Sistema. fuera. println("Estees el tercer mensaje" );
+        System.out.println("Este es mi tercer mensaje para fusionar");
     }
 }
